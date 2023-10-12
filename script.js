b51654af27130107039c480fe413859fc14ab31f
JunyPurple <ckkjunior77@gmail.com>
2023-10-12 20:22:07 +0300

    Added new files

---
+++ script.js
@@ -0,0 +1,20 @@
+// function sendEmail() {
+//     var recipient = document.getElementById("recipient").value;
+//     var subject = document.getElementById("subject").value;
+//     var message = document.getElementById("message").value;
+// function sendEmail() {
+//     var recipient = "ckkjunior77@gmail.com";  // Adresa de e-mail a destinatarului
+//     var subject = "Subiectul e-mailului";  // Subiectul e-mailului
+//     var message = "Acesta este conținutul mesajului. Puteți adăuga text aici.";  // Mesajul
+  
+//     MailApp.sendEmail(recipient, subject, message);
+  
+
+//     // Apelați aici funcția care trimite e-mailul utilizând Google Apps Script.
+//     // Puteți utiliza AJAX sau Fetch API pentru a efectua această solicitare.
+
+//     // Exemplu de avertizare în consolă pentru datele introduse:
+//     console.log("Destinatar: " + recipient);
+//     console.log("Subiect: " + subject);
+//     console.log("Mesaj: " + message);
+// }
